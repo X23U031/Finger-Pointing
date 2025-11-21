960c2a57eba3551ed2f81c6ef8564288454d10ed
x23u031 <x23u031@PC505N015.chiba-fjb.ac.jp>
2025-11-21 11:57:58 +0900

    うんち

--- maze/src/main/java/dao/RewardDAO.java
+++ maze/src/main/java/dao/RewardDAO.java
@@ -13,7 +13,7 @@ import dto.Reward;
 
 public class RewardDAO extends BaseDao {
 
-	//ぷっしゅ！
+	//庄司はあほ
 	// ★ Connectionを受け取る形に修正
 	public List<Reward> findAll(Connection con) {
 		List<Reward> rewardList = new ArrayList<>();
